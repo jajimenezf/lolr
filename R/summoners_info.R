58f0ed7604565a66fa725add120cf620795a200b
jajimenezf <jajimenezf@users.noreply.github.com>
2018-02-10 16:18:19 -0300

    summoners_info
    upload summoners_info function

---
+++ R/summoners_info.R
@@ -0,0 +1,60 @@
+#' Summoner's basic information
+#'
+#' This function allows you to get basic summoner's information.
+#' @param api_key API key provided by RIOT games (https://developer.riotgames.com/)
+#' @param summoners_name Summoner's name
+#' @param region Summoner's region (RU, KR, BR, OCE, JP, NA, EUNE, EUW, TR, LAN, LAS)
+#' @param data_format Data format on which the data will be output, available choices are json and data.frame
+#' @keywords "league of legends" "API"
+#' @export
+#' summoners_info(
+#' "you_api_key", 
+#' "Bjergsen", 
+#' "NA")
+
+summoners_info <- 
+  function(api_key, 
+           summoners_name, 
+           region = "LAN", 
+           data_format = "json") {
+    require(jsonlite)
+    require(dplyr)
+    require(devtools)
+    
+    # load("data/all_regions.RData")
+    # devtools::use_data(all_regions)
+    load("data/all_regions.rda")
+    
+    api_region <- 
+      all_regions %>%
+      filter(region_name == region) %>%
+      select(api_name)
+    
+    summoners_info <-
+      try(paste0(
+        "https://",
+        api_region,
+        ".api.riotgames.com/lol/summoner/v3/summoners/by-name/",
+        summoners_name, 
+        "?api_key=",
+        api_key
+      ) %>%
+        fromJSON(.), 
+      silent = T)
+    
+    if(inherits(summoners_info, 
+                "try-error")) {
+      return(paste0(
+        "account ",
+        summoners_name,
+        " not found, error = ", 
+        summoners_info[1]
+      ))
+    }
+    
+    if(data_format == "data.frame"){
+      do.call(cbind, summoners_info) %>%
+        return
+    } else
+      return(summoners_info)
+  }
